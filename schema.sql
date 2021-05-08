72f3ee5646e1d5671d13679850b629f0c60395bd
Cory Hisey <cjhisey@gmail.com>
2021-05-08 09:04:03 +0200

    removing files

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
-CREATE TABLE brews (
+CREATE TABLE brewys (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     brewery_name VARCHAR(255) NOT NULL,
     rating INT NOt NULL,
-    lat INT NOT NULL,
-    lng INT NOT NULL,
+    lat FLOAT NOT NULL,
+    lng FLOAT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 )
 
-INSERT INTO brews(Brewery_name, rating, lat, lng) VALUES
-('Craft Beer Market', 9, 50.12565, -118.6524);
+INSERT INTO brewys(Brewery_name, rating, lat, lng) VALUES
+('Copper Brewing', 9, 49.880529, -119.457427);
